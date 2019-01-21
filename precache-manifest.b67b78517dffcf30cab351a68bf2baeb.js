09cee227a1ab6956a06a61a3fa70fe1e9269ef36
denisyuk26 <sergey.denisyuk@gmail.com>
2019-01-21 20:15:10 +0200

    Updates

---
+++ precache-manifest.b67b78517dffcf30cab351a68bf2baeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a9daedd9335416e2d70f90ded0f3351",
+    "url": "/kobra/static/media/snake.2a9daedd.png"
+  },
+  {
+    "revision": "c26f6ebc86569250531b0f292e274ebc",
+    "url": "/kobra/static/media/grass.c26f6ebc.png"
+  },
+  {
+    "revision": "a1524b59ddbda4f7ec9567a7a822ad81",
+    "url": "/kobra/static/media/apple.a1524b59.png"
+  },
+  {
+    "revision": "f23f8b9c9da1cf7816c9",
+    "url": "/kobra/static/js/runtime~main.f23f8b9c.js"
+  },
+  {
+    "revision": "8bc3ac7945bbc548a0df",
+    "url": "/kobra/static/js/main.8bc3ac79.chunk.js"
+  },
+  {
+    "revision": "26f0bf2f6f11d273dfaa",
+    "url": "/kobra/static/js/1.26f0bf2f.chunk.js"
+  },
+  {
+    "revision": "8bc3ac7945bbc548a0df",
+    "url": "/kobra/static/css/main.043c3fbe.chunk.css"
+  },
+  {
+    "revision": "567f36361d22ad8ce79b62364e4f5cb3",
+    "url": "/kobra/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kobra/precache-manifest.f26ad6544ae1664b0ba378b296806d7b.js"
+  "/kobra/precache-manifest.b67b78517dffcf30cab351a68bf2baeb.js"
 );
 
 workbox.clientsClaim();
